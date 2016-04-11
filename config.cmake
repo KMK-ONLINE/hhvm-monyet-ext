39f05d1d4684816e7b31de046d64856a89d974a6
Fadhil Mandaga <firodj@gmail.com>
2016-04-11 11:32:40 +0700

    Move structure

--- config.cmake
+++ config.cmake
@@ -1,3 +1,3 @@
-HHVM_EXTENSION(monyet ext_monyet.cpp)
-HHVM_SYSTEMLIB(monyet ext_monyet.php)
+HHVM_EXTENSION(monyet hphp/runtime/ext/monyet/ext_monyet.cpp)
+HHVM_SYSTEMLIB(monyet hphp/runtime/ext/monyet/ext_monyet.php)
 
